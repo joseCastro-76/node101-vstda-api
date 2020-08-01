e5f78efd887abacf41ff73aeda59bf8a8c571ffd
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-08-01 08:50:09 -0700

    initial upload.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// add your code here
+const mock = [
+    {
+        todoItemId: 0,
+        name: 'an item',
+        priority: 3,
+        completed: false
+    },
+    {
+        todoItemId: 1,
+        name: 'another item',
+        priority: 2,
+        completed: false
+    },
+    {
+        todoItemId: 2,
+        name: 'a done item',
+        priority: 1,
+        completed: true
+    }
+]
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.status(200).type('application/json').send({status : 'ok'}));
+
+app.get('/api/TodoItems', (req, res) => res.status(200).send(mock));
+app.post('/api/TodoItems', (req, res) => {
+    const todo = req.body;
+    if(mock.filter(item => item.todoItemId === todo.todoItemId)) {
+        mock.splice(todo.todoItemId, 1, todo);
+    } else {
+        mock.push(todo)
+    }
+    res.status(201).send(todo);
+});
+
+app.get('/api/TodoItems/:number', (req, res) => {
+    const todo = mock.find(item => item.todoItemId == req.params.number)
+    res.status(200).send(todo);
+});
+app.delete('/api/TodoItems/:number', (req, res) => {
+    const deletedItem = mock.splice(req.params.number, 1);
+    res.status(200).send(deletedItem[0]);
+})
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
 const server = require('./app');
 
+const port = 8484;
+
 // write your code here
+server.listen(port, function() {
+    console.log(`Server is listening on http://localhost:${port}`);
+})
